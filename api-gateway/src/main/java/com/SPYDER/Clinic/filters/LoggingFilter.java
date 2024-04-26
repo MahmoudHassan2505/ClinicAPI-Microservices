0180ef39be8f8a7d306968f3704f9fbb57f96c84
Mahmoud Hassan <mshmoudghania@gmail.com>
2024-04-26 22:00:40 +0300

    Config filters in API Gateway MS

--- api-gateway/src/main/java/com/SPYDER/Clinic/filters/LoggingFilter.java
+++ api-gateway/src/main/java/com/SPYDER/Clinic/filters/LoggingFilter.java
@@ -1,4 +1,31 @@
 package com.SPYDER.Clinic.filters;
 
-public class LoggingFilter {
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    Logger logger = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Request Coming.....");
+
+        String request = exchange.getRequest().getURI().toString();
+        logger.info("URL: " +request);
+
+        //filter throw country name
+
+        String Ip = exchange.getRequest().getRemoteAddress().toString();
+        logger.info("IP: " +Ip);
+
+        return chain.filter(exchange);
+    }
 }
